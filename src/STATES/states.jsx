e7af55603ab822c681fbd6c1ca2bc0e75c65ac00
Suraj sharma <surajsharma15y@gmail.com>
2026-06-27 13:59:59 +0530

    Refactor states component with increment/decrement logic

--- src/STATES/states.jsx
+++ src/STATES/states.jsx
@@ -3,7 +3,66 @@ import React from 'react'
 const states = () => {
   return (
     <div>
+      import React, { useState } from 'react'
+import { useActionState } from 'react'
+
+
+const App = () => {
+  
+
+  //  const [num, setnum] = useState(20)
+
+
+  //     function clickview(){
+  //       console.log(setnum)
+
+  //       setnum(38)
+
+        //  console.log(setnum)
+
+const [num, setnum] = useState(0)
+
+if(change === 5){
+  console.log("CONGO , DEAR")
+}
+
+function change(){
+
+  setnum(num+1)
+
+  
+}
+function changetwo(){
+
+  setnum(num-1)
+}
+
+
       
+
+return (
+    <div>
+
+      {/* <h1>the value is {num}</h1> */}
+      {/* <button  onClick={clickview}>click here</button> */}
+
+
+      <button onClick={change}>INCREMENT</button>
+      <button  onClick={changetwo}>DECREMENT</button>
+
+      <p> {num}</p>
+
+   
+
+
+
+
+      
+    </div>
+  )
+
+}
+export default App
     </div>
   )
 }
